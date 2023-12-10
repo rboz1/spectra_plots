735558890f339b8e0b2cadde2eda066fc32cba29
rboz1 <63253421+rboz1@users.noreply.github.com>
2023-12-10 12:48:46 +0100

    test vs code github repos

--- spectroscopy.py
+++ spectroscopy.py
@@ -14,49 +14,53 @@ def my_name() -> str:
 
 def parse_spectrum(filename: str) -> list[float]:
     with open(filename, 'r+', encoding="utf-8") as f:
-        
         # read file lines into a list
         spectrum_line_list = f.readlines()
         
-        # remove header
+        # remove header lines from list
         spectrum_list_no_header = spectrum_line_list[11:]
         
         # split list of file lines based on space character to make a nested list
         spectrum_nested_list = [sublist.split(" ") for sublist in spectrum_list_no_header]
         
-        # loop through nested list and remove first element
+        # loop through nested list and remove first element which is the wavenumber
         spectrum_no_first_element = [sublist[1:] for sublist in spectrum_nested_list]
                 
         # flatten nested list
         spectrum_flattened_list = [element for sublist in spectrum_no_first_element for element in sublist]
         
         # remove "\n" character
-        # remove last list element which is a space
-        # turn list elements from strings to floats
         spectrum_no_newline = [element.replace("\n", "") for element in spectrum_flattened_list]
+        
+        # remove last list element which is a space from the split function used
         spectrum_no_newline.pop()
+        
+        # turn list elements from strings to floats
         spectrum_list = [float(element) for element in spectrum_no_newline]
         
     return spectrum_list
 
 
+def remove_invalid_transmittance(spectrum: list[float]) -> list[float]:
+    # remove out of range transmittance measurements if greater than 100 or less than 0
+    new_spectrum = [val for val in spectrum if 0 < val < 100]
+    return new_spectrum
+
+
 def absorbance(x: float) -> float:
-    
-    # transmittance outside of range
-    if x <= 0 or x > 100: 
-        return 0.0
-    # beer lambert law 
-    else:
-        absorbance_value = -math.log10(x / 100)
-        return absorbance_value
+    # beer lambert law to calculate absorbance from transmittance
+    absorbance_value = -math.log10(x / 100)
+    return absorbance_value
 
 
 def absorbance_list(spectrum: list[float]) -> list[float]:
-    
+    # initialize list to append absorbance values to
     absorbance_values_list = []
     
-    for i in spectrum:
-        absorbance_values_list.append(absorbance(i))
+    # loop through transmittance spectrum list and call absorbance function on each value
+    # append value to absorbance_values_list
+    for val in spectrum:
+        absorbance_values_list.append(absorbance(val))
         
     return absorbance_values_list
 
@@ -79,7 +83,7 @@ def plot_transmittance(spectrum: list[float]) -> None:
     plt.ylabel("Transmittance")
     plt.title("Pure Antibiotic Infrared Spectrum")
     
-    # plot limits
+    # plot x and y limits
     plt.xlim(4100,350)
     plt.ylim(10,110)
     
@@ -90,7 +94,7 @@ def plot_transmittance(spectrum: list[float]) -> None:
 
 
 def center_list(x: list[float]) -> list[float]:
-    # subtract mean of list from each element and save in new list
+    # subtract mean of entire absorbance list from each element and save in new list
     centered_data = [element - sum(x)/len(x) for element in x]
     
     return centered_data
@@ -139,20 +143,23 @@ if __name__ == "__main__":
     # plot_transmittance(pure_spectrum)
 
     # test case with one spectrum
-    # spectrum = parse_spectrum(spec_file_path_list[33])
+    spectrum = parse_spectrum(spec_file_path_list[91])
+    cat = remove_invalid_transmittance(spectrum)
+    print(spectrum)
+    print(cat)
     # spectrum_abs = absorbance_list(spectrum)
     # spectrum_centered = center_list(spectrum_abs)
+    
     
     # print(len(spectrum_centered))
     # print(len(pure_spectrum_centered))
     
-    for i in spec_file_path_list[1:]:
-        spectrum = parse_spectrum(i)
-        spectrum_abs = absorbance_list(spectrum)
-        spectrum_centered = center_list(spectrum_abs)
-        print(correlation(spectrum_centered, pure_spectrum_centered))
+    # for i in spec_file_path_list[1:]:
+    #     spectrum = parse_spectrum(i)
+        # spectrum_abs = absorbance_list(spectrum)
+        # spectrum_centered = center_list(spectrum_abs)
+        # corrs = correlation(spectrum_centered, pure_spectrum_centered)
         
-    
     
 # change file path
 # add docstrings
